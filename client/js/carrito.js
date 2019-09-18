f51588f061b3f093661220b48e8e06a5f7c67cea
Javier Dottori <javier.dottori@gmail.com>
2019-09-18 08:32:26 -0300

    Add PUT in front end

--- client/js/carrito.js
+++ client/js/carrito.js
@@ -2,6 +2,8 @@ let btnAgregar = document.querySelector("#btnAgregar");
 btnAgregar.addEventListener("click", agregar);
 let btnTotal = document.querySelector("#btnTotal");
 btnTotal.addEventListener("click", sumar);
+let btnEnvioEditar = document.querySelector("#btnEnvioEditar");
+btnEnvioEditar.addEventListener("click", envioEditar);
 
 let compras = [];
 
@@ -71,11 +73,16 @@ function mostrarTablaCompras() {
             <tr>
                 <td>${r.nombreProducto}</td>
                 <td>${r.precio}</td>
+                <td><button class="btn-editar-producto" pos="${i}">Editar</button></td>
                 <td><button class="btn-delete-producto" pos="${i}">Borrar</button></td>
             </tr>
         `; //la comilla es el acento inverso
     }
     document.querySelector("#tblCompras").innerHTML = html;
+    let botonesEditar = document.querySelectorAll(".btn-editar-producto");
+    botonesEditar.forEach(e => {
+      e.addEventListener("click", btnEditarClick);
+    }); 
     let botonesBorrar = document.querySelectorAll(".btn-delete-producto");
     botonesBorrar.forEach(e => {
       e.addEventListener("click", btnBorrarClick);
@@ -91,6 +98,39 @@ async function btnBorrarClick(){
     "headers": {
       "Content-Type": "application/json"
     }
+  })
+  load();
+}
+
+async function btnEditarClick(){
+  let pos = this.getAttribute("pos");
+  let r = await fetch("/productos/"+pos);
+  let json = await r.json();
+
+  document.querySelector('#producto').value = json.nombreProducto;
+  document.querySelector('#precio').value = json.precio;
+  document.querySelector('#pos-elem').value = pos;
+  document.querySelector('#btnEnvioEditar').classList.remove("oculto");
+  document.querySelector('#btnAgregar').classList.add("oculto");
+  
+}
+
+async function envioEditar(){
+  document.querySelector('#btnEnvioEditar').classList.add("oculto");
+  document.querySelector('#btnAgregar').classList.remove("oculto");
+  let producto = document.querySelector('#producto').value;
+  let precio = parseInt(document.querySelector('#precio').value);
+  let renglon = {
+    "nombreProducto": producto,
+    "precio": precio
+  }
+  let pos = document.querySelector('#pos-elem').value;
+  let response = await fetch(`/productos/${pos}`, {
+    "method": "PUT",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "body": JSON.stringify(renglon)
   })
   load();
 }
